0d174d8270ed9e0b7e491a1b0526cbb62360860e
Sử Thành Công <stc17022002@gmail.com>
2021-05-11 12:18:01 +0700

    meo

--- R/R1105.R
+++ R/R1105.R
@@ -1,30 +1,40 @@
 # Bài tập ngày 11/05
 setwd("C:/Users/ADMIN/Desktop/GiaiThuat-R/R/data")
-owls = read.table(file = "Owls.txt", header = TRUE)
+owls = read.table(file = "owls.txt", header = TRUE)
 names(owls)
 str(owls)
 
-library(dplyr)
+#library(dplyr)
 #A. Vẽ biểu đồ tương quan giữa (SiblingNegotiation)với(ArrivalTime) cho cùng 1 tổ (Nest)
 Allnests = unique(owls$Nest)
 setwd("C:/Users/ADMIN/Desktop/GiaiThuat-R/R/data/image")
 for (i in 1:27){
   Nest.i = Allnests[i]
   owls.i = owls[owls$Nest == Nest.i,]
+  YourFileName = paste(Nest.i,".jpg",sep = "")# tên file lưu vào máy
+  jpeg(file = YourFileName)
   plot(x = owls.i$ArrivalTime, y = owls.i$SiblingNegotiation,
        xlab = "Arrival Time",
        main = Nest.i,
        ylab = "Sibling Negotiationr")
+  dev.off()
 }
 
 
 #B. 
-ifelse(owls$FoodTreatment == "Satiated", owls$NestNight <- paste(owls$ï..Nest, "1",sep = "_"), owls$NestNight <- paste(owls$ï..Nest, "2",sep = "_"))
+help("ifelse")
+N=length(owls$Nest)
+for (i in 1:N){
+  ifelse(owls$FoodTreatment[i] == 'Deprived', #Điều kiện
+         owls$NestNight[i] <- paste(owls$Nest[i], "2",sep = "_"), #Lệnh thực hiện nếu đúng
+         owls$NestNight[i] <- paste(owls$Nest[i], "1",sep = "_")) #Lệnh thực hiện nếu sai
+}
 # hàm ifelse khi FoodTreatment là satiated thì paste vào cột mới là NestNight 
 # dữ liệu là cột Nest và _1 và ngược lại là _2
 head(owls)#in ra những cột đầu
 
 AllNestsNights= unique(owls$NestNight)
+AllNestsNights
 N=length(AllNestsNights)  # Các phần tử trong AllNestsNights
 for (i in 1:N){
   NestNight.i = as.character(AllNestsNights[i]) # đưa về dạng character
