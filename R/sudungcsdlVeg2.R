07d2951b98a932180fc593c31a5cf799e783cf3f
Sử Thành Công <stc17022002@gmail.com>
2021-05-17 18:34:01 +0700

    meo

--- R/sudungcsdlVeg2.R
+++ R/sudungcsdlVeg2.R
@@ -1,61 +1,45 @@
 #Thiết lập thư mục hiện hành
-setwd("D:\\Giang Day\\R Programming\\Chapter 6")
-#RIKZ.txt
-Benthic <- read.table("RIKZ.txt", header = TRUE)
-names(Benthic)
-str(Benthic)
-#Biến Species (loài) từ cột thứ 2 đến 76 trong biến Benthic
-Species <- Benthic[,2:76]
+setwd("C:/Users/ADMIN/Desktop/GiaiThuat-R/R/data")
+
+#Du lieu:
+dt = read.table('Vegetation2.txt',header = TRUE)
+names(dt)
+str(dt)
+#Bien Species(loai) tu cot thu 5 den 9 trong dt
+Species = dt[,5:9]
 names(Species)
-str(Species)
-#Kiểm tra dimensions - chiều của biến Species
-n <- dim(Species)
+#Kiem tra dimensions -chieu cua bien Species
+n = dim(Species)
 n
-# 45 dòng - tương ứng với 45 sites - vị trí thu thập mẫu vật
-# Tương ứng với mỗi vị trí thì sẽ ghi nhận được những loài
-#sinh vật biển nào?
-#Có bao nhiêu loài sinh vật biển ở vị trí số 1
-sum(Species[1,], na.rm = TRUE)
-#Kết quả 143 loài sinh vật biển xuất hiện ở site #1
-sum(Species[2,], na.rm = TRUE)
-#... Làm 45 lần để kiểm tra số lượng sinh vật biển
-#xuất hiện tại các sites
-TA <- vector(length = n[1])
-for (i in 1:n[1]) {
-  TA[i] <- sum(Species[i,], na.rm = TRUE)
+sum(Species[1,], na.rm=TRUE)
+sum(Species[2,], na.rm=TRUE)
+#...............
+TA = vector(length = n[1])
+for (i in 1:n[1]){
+  TA[i]=sum(Species[i,],na.rm=TRUE)
 }
+#Ham rowSums tinh tong cac cot trong ham
+#na.rm =TRUE loai bo cac gia tri rong
 TA
-#Hàm rowSums: tính toán tổng của các cột ở trong hàng
-#tham số na.rm=TRUE => Loại bỏ các giá trị rỗng
-
-
-TA02 <- rowSums(Species, na.rm = TRUE)
+TA02 = rowSums(Species,na.rm=TRUE)
 TA02
-#Đi vào chi tiết tại từng vị trí quan sát
-sum(Species[1,]>0, na.rm = TRUE)
-#Kết quả chúng ta có 11 loài khác nhau xuất hiện tại vị trí 1
-Richness <- vector(length=n[1])
-for (i in 1:n[1]) {
-  Richness[i] <- sum(Species[i,]>0, na.rm=TRUE)
+#di vao chi tiet tung vi tri quan sat
+sum(Species[1,]>0,na.rm=TRUE)
+Richness = vector(length = n[1])
+for (i in 1:n[1]){
+  Richness[i]=sum(Species[i,]>0,na.rm=TRUE)
 }
 Richness
-
-
-Richness02 <- rowSums(Species>0, na.rm = TRUE)
+Richness02=rowSums(Species>0,na.rm=TRUE)
 Richness02
+#chi so da dang loai
+#H=sum chay ru i -> m cua bien p: xac suat*log10 p
+RS = rowSums(Species,na.rm =TRUE)
+prop=Species/RS
 
-
-# Chỉ số đa dạng loài
-#https://en.wikipedia.org/wiki/Diversity_index
-#Tìm hiểu cách tính chỉ số đa dạng loài
-#10h40.
-#H - sum biến chạy từ i -> m của biến p: xác xuất * log10 p
-RS <- rowSums(Species, na.rm=TRUE)
-prop <- Species / RS
-H <- -rowSums(prop * log10(prop), na.rm= TRUE)
+H = -rowSums(prop * log10(prop), na.rm= TRUE)
 H
-# Kết hợp lại hết những gì đã làm vô 1 hàm của riêng mình
-# sau này sử dụng lại để phân tích những dữ liệu tương tự
+#Ket hop lai lam 1 ham
 Index.function <-function(Spec, Choice){
   if (Choice=="Richness") {
     Index <- rowSums(Species>0, na.rm = TRUE)
@@ -70,3 +54,7 @@ Index.function <-function(Spec, Choice){
   }
   list(Index=Index, MyChoice= Choice)
 }
+#chayham
+Index.function(Species,"Shannon")
+Index.function(Species,"Total Abundance")
+Index.function(Species,"Richness")
