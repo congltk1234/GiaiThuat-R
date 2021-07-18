02973026c21f6e14b742134478e5222c3e25a24f
Sử Thành Công <stc17022002@gmail.com>
2021-07-18 21:12:56 +0700

    meo

--- CTDLGT_R/BT14_SelectionSort.R
+++ CTDLGT_R/BT14_SelectionSort.R
@@ -1,28 +1,12 @@
-# function to sort array using selection sort
-selection_sort <- function(x)
-{
-  # length of array
-  n <- length(x)
-  for (i in 1 : (n - 1))
-  {
-    # assume element at i is minimum
-    min_index <- i
-    for (j in (i + 1) : (n))
-    {
-      # check if element at j is smaller
-      # than element at min_index
-      if (x[j] < x[min_index]) {
-        # if yes, update min_index
-        min_index = j
-      }
-    }
-    # swap element at i with element at min_index
-    temp <- x[i]
-    x[i] <- x[min_index]
-    x[min_index] <- temp
+selectionSort <- function(arr) {
+  smallest <- min(arr)
+  rest <- arr[arr != smallest]
+  
+  if (length(rest) > 1) {
+    rest <- selectionSort(arr[arr != smallest])
   }
-  x
+  
+  c(smallest, rest)
 }
-
 x <- c(23,43,2122,3132,12,21,44,12,99,12,4,77) 
-selection_sort(x)
+selectionSort(x)
